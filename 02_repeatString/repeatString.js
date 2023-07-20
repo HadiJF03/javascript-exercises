8f67fc76c428a05d05d496ede1595224c57032a5
HadiJF03 <48181301+HadiJF03@users.noreply.github.com>
2023-07-20 15:35:26 +0300

    passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = "";
+    if (string == "") return string;
+    if (num == 0) return repeatedString;
+    if (num<0) return "ERROR";
+    while(num>0){
+        repeatedString += string;
+        num--;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number" || num1<0 || num2<0) return "ERROR";
+    let sum = 0;
+    let largest = num1>num2 ? num1:num2;
+    let smallest = num1<num2 ? num1:num2;
+    for(i =1; i<=largest;i++){
+        sum += i; 
+        sum += smallest>i ? i:0;
+    }
+    return sum;
 
 };
 
